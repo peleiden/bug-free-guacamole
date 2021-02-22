cd86306925bbc4e7310d45388db1b28c63a4349c
Søren Winkel Holm <swholm@protonmail.com>
2021-02-22 11:20:51 +0100

    TABles

--- scripts/da-NER-textables.py
+++ scripts/da-NER-textables.py
@@ -1,30 +1,53 @@
 import sys, os
 import json
-from latextable import Texttable
+
+from latextable import Texttable, draw_latex
+import pyperclip
+
+
 
 DATASETS = ("DaNE", "Plank", "WikiANN")
 
+def f1f(f: float) -> str:
+    """ Format F1-score """
+    if f  == "-": return f
+    return "%.2f" % (f*100)
+
+
 def build_main_table(dataset: str, experiment: dict) -> str:
-    print(dataset)
     t = Texttable()
+    t.set_deco(t.HEADER)
     cats = ["LOC", "PER", "ORG"]
     miscdata = dataset != "WikiANN"
-    if miscdata:
-        cats.append("MISC")
-
-    t.set_cols_align(["l", "l"] + ["r"]*(2+len(cats)))
     # Fix miscfuckeri
-    t.add_row( ["Model", "Train data", "Micro avg.", "Micro avg. (no MISC)", *cats] )
+    row = ["Model", "Train data", "Micro avg."]
+    if miscdata:
+        row.append("w/o MISC")
+        cats.append("MISC")
+    row += cats
+
+    t.set_cols_align(["l", "l"] + ["r"]*(1+len(cats)+int(miscdata)))
+    t.set_cols_dtype(["t"]*len(row)) # Dont overwrite my formatting pls
+    t.header(row)
+
     for mname in sorted(experiment, key=str.lower):
         v = experiment[mname]
-        miscmodel = miscdata and v["stats"]["MISC"]["f1-score"] != 0
-        t.add_row(
-            [mname, "", v["stats"]["micro avg"]["f1-score"] if miscmodel else "-", v["stats_nomisc"]["micro avg"]["f1-score"]]
-                + [v["stats"][c]["f1-score"] for c in cats]
-        )
-
-    print(t.draw())
-
+        row = [mname, ""]
+        if miscdata:
+            row.append(f1f(v["stats"]["micro avg"]["f1-score"]) or "-")
+        row.append(f1f(v["stats_nomisc"]["micro avg"]["f1-score"]))
+        row += [f1f(v["stats"][c]["f1-score"] or "-") for c in cats]
+        t.add_row(row)
+    row = ["Support", "", v["stats"]["micro avg"]["support"]]
+    if miscdata:
+        row.append(v["stats_nomisc"]["micro avg"]["support"])
+    row += [v["stats"][c]["support"] for c in cats]
+    t.add_row(["" for _ in row])
+    t.add_row(row)
+    #print(t.draw())
+    out = draw_latex(t, caption=f"F1\pro-scores of Danish NER models of the {dataset} data-set consisting of {v['N']} sentences.", label=f"tab:{dataset}")
+    print(out)
+    return out
 
 if __name__ == '__main__':
     indir = sys.argv[1]
@@ -34,6 +57,13 @@ if __name__ == '__main__':
             model, dataset = folder.split("-")
             with open(os.path.join(indir, folder, "data.json"), "r") as f:
                 experiment = json.load(f)
-            experiments[dataset][model] = {"stats": experiment["statistics"], "stats_nomisc": experiment["statistics_nomisc"]}
-    for data, exp in experiments.items():
-        build_main_table(data, exp)
+            experiments[dataset][model] = {
+                    "stats": experiment["statistics"],
+                    "stats_nomisc": experiment["statistics_nomisc"],
+                    "N": len(experiment["predictions"])
+                }
+    pyperclip.copy("\n\n".join(
+        build_main_table(data, exp) for data, exp in experiments.items()
+        )
+    )
+
---
+++ scripts/en-LUKE-textables.py
@@ -0,0 +1,4 @@
+import sys, os
+
+if __name__ == '__main__':
+    indir = sys.argv[1]
